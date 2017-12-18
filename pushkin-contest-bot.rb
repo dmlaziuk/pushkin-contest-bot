ef06b5a9191b1d654158dd1f30093a94042a477f
Dzmitry Laziuk <dm.laziuk@gmail.com>
2017-12-18 03:04:50 +0300

    rack

--- pushkin-contest-bot.rb
+++ pushkin-contest-bot.rb
@@ -1,14 +1,35 @@
 require 'yaml'
+require 'net/http'
+require 'rack'
 require_relative 'lib/pushkin'
 
-pushkin = Pushkin.new
-puts 'loading lyrics.yml ...'
-verses = YAML.load(File.read('lyrics.yml'))
-puts 'adding verses ...'
-verses.each do |verse|
-  pushkin.add(verse[0], verse[1])
+class PushkinContestBot
+  def initialize
+    @token = '7cb21edad8e21d1abf2147b12f89f276'
+    @pushkin = Pushkin.new
+    verses = YAML.load(File.read('lyrics.yml'))
+    verses.each do |verse|
+      @pushkin.add(verse[0], verse[1])
+    end
+    @pushkin.init_hash
+  end
+
+  def self.call(env)
+    req = Rack::Request.new(env)
+    puts 'REQ'
+    p req
+    puts 'PARAMS'
+    p req.params
+    puts 'GET'
+    p req.GET
+    Rack::Response.new("OK", 200)
+#    answer = pushkin.run_level1('Он уважать себя заставил')
+#    uri = URI("http://pushkin.rubyroidlabs.com/quiz")
+#    parameters = {
+#      answer: "#{answer}",
+#      token: "#{token}",
+#      task_id: "#{task_id}"
+#    }
+#    Net::HTTP.post_form(uri, parameters)
+  end
 end
-puts 'initializing hash ...'
-pushkin.init_hash
-puts 'runing level1 ...'
-puts pushkin.run_level1('Он уважать себя заставил')
