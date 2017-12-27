46d278ecb79cfe8cd5d8d14b7768dcd0feffda24
Dzmitry Laziuk <dm.laziuk@gmail.com>
2017-12-27 03:06:47 +0300

    benchmark

--- parse3.rb
+++ parse3.rb
@@ -1,4 +1,7 @@
+require 'benchmark'
 require 'mechanize'
+
+include Benchmark
 
 URL = 'http://ilibrary.ru/author/pushkin/l.all/index.html'.freeze
 
@@ -10,45 +13,50 @@ page = agent.get(URL)
 links = page.xpath('//div[@class="list"]/p')
 total = links.size
 count = 1
-links.each do |link|
-#  print "\r#{count}/#{total}"
-  title = link.text.gsub(/ \(.*?\)/, '')
-  title.gsub!('', '')
-  title.gsub!(/\s+/, ' ')
-  quote = title.scan(/«(.*?)...»/)
-  title = quote[0][0] unless quote.empty?
-  puts "TITLE:#{title}"
-  url = "http://ilibrary.ru#{link.xpath('./a').attribute('href')}"
-  url.gsub!('index.html', 'p.1/index.html')
-  page = agent.get(url)
-  ttl = nil
-  begin
-    ttl ||= page.xpath('//div[@class="title"]/h1').text
-    ttl = title if ttl.empty?
-    ttl = title if ttl.include?('*')
-    ttl.gsub!(' ', '')
-    ttl.gsub!(/\d+$/, '')
-    ttl.gsub!(/\s+/, ' ')
-    puts "  TTL:#{ttl}"
-    txt = page.xpath('//span[@class="vl"]')
-    if txt.any?
-      arr = []
-      txt.each do |i|
-        line = i.text
-        line.gsub!(' ', ' ')
-        line.gsub!("\u0097", ' ')
-        line.gsub!(/\d+$/, ' ')
-        line.gsub!(/ \d+.$/, '.')
-        line.gsub!(/\s+/, ' ')
-        line.strip!
-        arr << line
-      end
-      verses << [ttl, arr]
+time = Tms.new
+total_time = Tms.new
+benchmark(CAPTION, 11, FORMAT, ' Total time', 'Averge time') do |bm|
+  links.each do |link|
+    time = bm.report(format('%4d / %4d', count, total)) do
+      title = link.text.gsub(/ \(.*?\)/, '')
+      title.gsub!('', '')
+      title.gsub!(/\s+/, ' ')
+      quote = title.scan(/«(.*?)...»/)
+      title = quote[0][0] unless quote.empty?
+      url = "http://ilibrary.ru#{link.xpath('./a').attribute('href')}"
+      url.gsub!('index.html', 'p.1/index.html')
+      page = agent.get(url)
+      title2 = nil
+      begin
+        title2 ||= page.xpath('//div[@class="title"]/h1').text
+        title2 = title if title2.empty?
+        title2 = title if title2.include?('*')
+        title2.gsub!(' ', '')
+        title2.gsub!(/\d+$/, '')
+        title2.gsub!(/\s+/, ' ')
+        txt = page.xpath('//span[@class="vl"]')
+        if txt.any?
+          arr = []
+          txt.each do |i|
+            line = i.text
+            line.gsub!(' ', ' ')
+            line.gsub!("\u0097", ' ')
+            line.gsub!(/\d+$/, ' ')
+            line.gsub!(/ \d+.$/, '.')
+            line.gsub!(/\s+/, ' ')
+            line.strip!
+            arr << line
+          end
+          verses << [title2, arr]
+        end
+        next_page = page.link_with(xpath: '//a[@title="Дальше"]')
+        page = next_page.click if next_page
+      end while next_page
+      count += 1
     end
-    next_page = page.link_with(xpath: '//a[@title="Дальше"]')
-    page = next_page.click if next_page
-  end while next_page
-  count += 1
+    total_time += time
+  end
+  [total_time, total_time / (count - 1)]
 end
 
 File.open('lyrics3.yml', 'w') { |f| f.write(YAML.dump(verses)) }
--- pushkin-contest-bot.rb
+++ pushkin-contest-bot.rb
@@ -1,6 +1,9 @@
+require 'benchmark'
 require 'yaml'
 require 'net/http'
 require_relative 'lib/pushkin'
+
+include Benchmark
 
 TOKEN = '7cb21edad8e21d1abf2147b12f89f276'.freeze
 MY_URI = URI('http://pushkin.rubyroidlabs.com/quiz')
@@ -8,11 +11,19 @@ MY_URI = URI('http://pushkin.rubyroidlabs.com/quiz')
 class PushkinContestBot
   def initialize
     @pushkin = Pushkin.new
-    verses = YAML.load(File.read('lyrics3.yml'))
-    verses.each do |verse|
-      @pushkin.add(verse[0], verse[1])
+    @count = [0]*9
+    @time = Tms.new
+    @total_time = [Tms.new]*9
+    verses = []
+    benchmark(CAPTION, 10, FORMAT) do |bm|
+      bm.report('Load YAML:') { verses = YAML.load(File.read('lyrics3.yml')) }
+      bm.report('Add2array:') do
+        verses.each do |verse|
+          @pushkin.add(verse[0], verse[1])
+        end
+      end
+      bm.report('Init hash:') { @pushkin.init_hash }
     end
-    @pushkin.init_hash
   end
 
   def call(env)
@@ -24,21 +35,45 @@ class PushkinContestBot
     puts "Question:#{question}"
     answer = case level
     when 1
-      @pushkin.run_level1(question)
+      @count[1] += 1
+      @time = measure("Level 1/#{@count[1]}") { @pushkin.run_level1(question) }
+      @total_time[1] += @time
     when 2
-      @pushkin.run_level2(question)
+      @count[2] += 1
+      first ||= puts "Level 1: total=#{@total_time[1]} average=#{@total_time[1]/@count[1]}"
+      @time = measure("Level 2/#{@count[2]}") { @pushkin.run_level2(question) }
+      @total_time[2] += @time
     when 3
-      @pushkin.run_level3(question)
+      @count[3] += 1
+      second ||= puts "Level 2: total=#{@total_time[2]} average=#{@total_time[2]/@count[2]}"
+      @time = measure("Level 3/#{@count[3]}") { @pushkin.run_level3(question) }
+      @total_time[3] += @time
     when 4
-      @pushkin.run_level4(question)
+      @count[4] += 1
+      third ||= puts "Level 3: total=#{@total_time[3]} average=#{@total_time[3]/@count[3]}"
+      @time = measure("Level 4/#{@count[4]}") { @pushkin.run_level4(question) }
+      @total_time[4] += @time
     when 5
-      @pushkin.run_level5(question)
+      @count[5] += 1
+      fourth ||= puts "Level 4: total=#{@total_time[4]} average=#{@total_time[4]/@count[4]}"
+      @time = measure("Level 5/#{@count[5]}") { @pushkin.run_level5(question) }
+      @total_time[5] += @time
     when 6
-      @pushkin.run_level6(question)
+      @count[6] += 1
+      fifth ||= puts "Level 5: total=#{@total_time[5]} average=#{@total_time[5]/@count[5]}"
+      @time = measure("Level 6/#{@count[6]}") { @pushkin.run_level6(question) }
+      @total_time[6] += @time
     when 7
-      @pushkin.run_level7(question)
+      @count[7] += 1
+      sixth ||= puts "Level 6: total=#{@total_time[6]} average=#{@total_time[6]/@count[6]}"
+      @time = measure("Level 7/#{@count[7]}") { @pushkin.run_level7(question) }
+      @total_time[7] += @time
     when 8
-      @pushkin.run_level8(question)
+      @count[8] += 1
+      seventh ||= puts "Level 7: total=#{@total_time[7]} average=#{@total_time[7]/@count[7]}"
+      @time = measure("Level 8/#{@count[8]}") { @pushkin.run_level8(question) }
+      @total_time[8] += @time
+      puts "Level 8: total=#{@total_time[8]} average=#{@total_time[8]/@count[8]}" if count[8] > 98
     else
       'нет'
     end
